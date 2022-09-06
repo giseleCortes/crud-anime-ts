d9f7b8c9a4ed6f4f08a07b69707c4843d23d045d
giseleCortes <86819936+giseleCortes@users.noreply.github.com>
2022-09-05 23:33:15 -0300

    layout base 2

--- src/shared/layouts/LayoutBasedePagina.tsx
+++ src/shared/layouts/LayoutBasedePagina.tsx
@@ -1,4 +1,5 @@
 import { Box } from '@mui/system';
+import { IconButton, Icon, Typography, useTheme } from '@mui/material';
 import { ReactNode } from 'react';
 
 
@@ -12,12 +13,18 @@ interface ILayoutBasedePaginaProps {
 
 }
 export const LayoutBasedePagina: React.FC<ILayoutBasedePaginaProps> = ({ children, titulo }) => {
-
+  const theme = useTheme();
 
   return (
     <Box height='100%' display='flex' flexDirection='column' gap={1}> 
-      <Box >
-        {titulo} 
+      <Box padding={1} display='flex' alignItems='center' height={theme.spacing(12)} gap={1}>
+        <IconButton>
+          <Icon>menu</Icon>
+        </IconButton>
+        <Typography variant = 'h5'>
+          {titulo}    
+        </Typography>
+        
       </Box>
       <Box >
         Barra de Ferramentas
