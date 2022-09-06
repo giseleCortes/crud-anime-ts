1de8a1544f15417dc07a6267071da75d85313df6
giseleCortes <86819936+giseleCortes@users.noreply.github.com>
2022-09-05 21:26:34 -0300

    Revert "Revert "layoutresponsivo""
    This reverts commit ce54cf5d87da2ae0ea749ebf4839630e5821ed64.

--- src/pages/dashboard/DashBoard.tsx
+++ src/pages/dashboard/DashBoard.tsx
@@ -3,7 +3,7 @@ import { LayoutBasedePagina } from '../../shared/layouts';
 export const Dashboard = () => {
 
   return (
-    <LayoutBasedePagina titulo='Pagina Inicial'>
+    <LayoutBasedePagina titulo='Pagina Inicial' barraDeFerramentas={<>Barra de Ferramentas</>}>
         testando
     </LayoutBasedePagina>
   );
