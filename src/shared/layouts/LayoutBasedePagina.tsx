ff88f563cf389de77224c75aebdfb5f04cd75130
giseleCortes <86819936+giseleCortes@users.noreply.github.com>
2022-09-05 22:17:58 -0300

    layout base de pagina 1

--- src/shared/layouts/LayoutBasedePagina.tsx
+++ src/shared/layouts/LayoutBasedePagina.tsx
@@ -1,51 +1,32 @@
-import { Icon, IconButton, Theme, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { Box } from '@mui/system';
 import { ReactNode } from 'react';
-import { useDrawerContext } from '../contexts';
+
 
 interface ILayoutBasedePaginaProps {
-    titulo: string;
-    children: ReactNode;
-    barraDeFerramentas: ReactNode;
+  children: ReactNode;
+  titulo: string;
+
+
+
 
 
 }
-export const LayoutBasedePagina: React.FC <ILayoutBasedePaginaProps> = ({ children, titulo, barraDeFerramentas }) => {
-  const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
-  const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
-  const theme = useTheme();
+export const LayoutBasedePagina: React.FC<ILayoutBasedePaginaProps> = ({ children, titulo }) => {
 
-  const {toggleDrawerOpen} = useDrawerContext();
 
   return (
-    <Box height='100%' display="flex" flexDirection="column" gap = {1}>
-      <Box padding={1} display="flex" alignItems="center" gap={1} height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)}>
-        {smDown && (
-          <IconButton onClick={toggleDrawerOpen}>
-            <Icon>menu</Icon>
-          </IconButton>
-        )}
-        
-
-
-        < Typography variant={smDown ? 'h5' : mdDown? 'h4' : 'h3'} 
-          overflow='hidden'
-          whiteSpace='nowrap'
-          textOverflow='ellipsis'>
-          {titulo}
-        </Typography>
-        
+    <Box height='100%' display='flex' flexDirection='column' gap={1}> 
+      <Box >
+        {titulo} 
       </Box>
+      <Box >
+        Barra de Ferramentas
+      </Box>  
+      <Box >
+        {children} 
+      </Box>  
 
-      {barraDeFerramentas && (
-        <Box>
-          {barraDeFerramentas}
-        </Box>
-      )}
-
-      <Box flex = {1} overflow='auto'>
-        { children }
-      </Box>      
+           
     </Box>
   );
 };
