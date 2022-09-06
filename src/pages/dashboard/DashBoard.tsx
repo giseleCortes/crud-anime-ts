b5597e4eb5b1efba48a678bb641425a61dea802c
giseleCortes <86819936+giseleCortes@users.noreply.github.com>
2022-09-05 21:24:10 -0300

    Revert "Revert "menu lateral e layout base""
    This reverts commit 0850b49517e9321321db04e0991500cdb9e3a0d0.

---
+++ src/pages/dashboard/DashBoard.tsx
@@ -0,0 +1,10 @@
+import { LayoutBasedePagina } from '../../shared/layouts';
+
+export const Dashboard = () => {
+
+  return (
+    <LayoutBasedePagina titulo='Pagina Inicial'>
+        testando
+    </LayoutBasedePagina>
+  );
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,25 @@
-import { Button } from '@mui/material';
+import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { Dashboard } from '../pages';
 
 import { useDrawerContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
-  const { toggleDrawerOpen } = useDrawerContext();
+  const { setDrawerOptions } = useDrawerContext();
+
+  useEffect(() => {
+    setDrawerOptions([
+      {
+        label:'Página inicial',
+        icon: 'home',
+        path: '/paginal-inicial',
+      },
+    ]);
+  }, []);
 
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}>Toggle drawer</Button>} />
+      <Route path="/pagina-inicial" element={<Dashboard />} />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
