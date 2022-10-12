4daeb6ae9ae7b6db561ae927ce9aa639372e4317
giseleCortes <86819936+giseleCortes@users.noreply.github.com>
2022-10-12 01:58:26 -0300

    variaveis de ambiente

--- src/shared/components/ferramentas-da-listagem/FerramentasDaListagem.tsx
+++ src/shared/components/ferramentas-da-listagem/FerramentasDaListagem.tsx
@@ -1,5 +1,7 @@
 import { Box, Button, Icon, Paper, TextField, useTheme } from '@mui/material';
 
+import { Enviroment } from '../../enviroment';
+
 interface IFerramentasDaListagemProps{
     textoDaBusca?: string;
     mostrarInputBusca?: boolean;
@@ -35,7 +37,7 @@ export const FerramentasDaListagem: React.FC<IFerramentasDaListagemProps> = ({
           size='small'
           value={textoDaBusca}
           onChange={(e) => aoMudarTextoDeBusca?.(e.target.value)}
-          placeholder='Pesquisar'
+          placeholder={Enviroment.INPUT_DE_BUSCA}
         />
       )}
 
