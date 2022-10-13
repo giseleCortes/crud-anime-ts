fe156a90562bde6e67c514a926d88ee952292f24
giseleCortes <86819936+giseleCortes@users.noreply.github.com>
2022-10-12 23:01:56 -0300

    service configurado

--- src/shared/services/api/animes/AnimesService.ts
+++ src/shared/services/api/animes/AnimesService.ts
@@ -44,13 +44,62 @@ const getAll = async (page = 1, filter= ''): Promise<TAnimesComTotalCount | Erro
     
 };
 
-const getById = async (): Promise<any> => {};
+const getById = async (id: number): Promise<IDetalheAnimes | Error> => {
+  try{
+    const {data} = await Api.get(`/animes/${id}`);
+    
+    if(data) {
+      return data;
 
-const create= async (): Promise<any> => {};
+    }
+    
 
-const updateById= async (): Promise<any> => {};
+    return new Error('Erro ao consultar o registro.');
 
-const deleteById= async (): Promise<any> => {};
+  } catch(error){
+
+    console.error(error);
+    return new Error((error as {message:string}). message ||'Erro ao consultar o registro.');
+  }
+};
+
+const create= async (dados: Omit<IDetalheAnimes, 'id'>): Promise<number | Error> => {
+  try{
+    const {data} = await Api.post<IDetalheAnimes>('/animes', dados);
+    
+    if(data) {
+      return data.id;
+    }
+    
+
+    return new Error('Erro ao criar o registro.');
+
+  } catch(error){
+
+    console.error(error);
+    return new Error((error as {message:string}). message ||'Erro ao criar o registro.');
+  }
+};
+
+const updateById= async (id: number, dados: IDetalheAnimes): Promise<void | Error> => {
+  try{
+    const {data} = await Api.put(`/pessoas/${id}`, dados);
+  } catch(error){
+
+    console.error(error);
+    return new Error((error as {message:string}). message ||'Erro ao atualizar o registro.');
+  }
+};
+
+const deleteById= async (id: number): Promise<void | Error> => {
+  try{
+    await Api.delete(`/pessoas/${id}`);
+  } catch(error){
+
+    console.error(error);
+    return new Error((error as {message:string}). message ||'Erro ao apagar o registro.');
+  }
+};
 
 
 export const AnimesService = {
